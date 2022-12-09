717604fd133f5512c7c336ef9160d6536dd0ae7b
Petru <petru@Petrus-MacBook-Pro.local>
2022-12-09 11:15:30 +0200

    cname added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,12 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Starter Blog`,
-        author: {
-            name: `Kyle Mathews`,
-            summary: `who lives and works in San Francisco building useful things.`
-        },
-        description: `A starter blog demonstrating what Gatsby can do.`,
-        siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-        social: {
-            twitter: `kylemathews`
-        }
+        title: 'PeterBrw',
+        siteUrl: 'https://peterbrw.online/',
+        description: 'Testing Site'
     },
     plugins: [
         `gatsby-plugin-image`,
+        'gatsby-plugin-cname',
         'gatsby-plugin-postcss',
         {
             resolve: 'gatsby-plugin-react-svg',
